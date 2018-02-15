c3335ab1a4dc63c7977069a0f7769f8c26970f82
bella148 <bella148@morris.umn.edu>
2018-02-15 14:55:21 -0600

    app.module.ts included proper paths for todo-list.component and other todo dependencies
    app.routes.ts
    added a path for todo-list.component
    
    -Blake and Roch

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,14 +7,15 @@ import {MATERIAL_COMPATIBILITY_MODE} from '@angular/material';
 import {AppComponent} from './app.component';
 import {HomeComponent} from './home/home.component';
 import {UserListComponent} from './users/user-list.component';
+import {TodoListComponent} from "./todos/todo-list.component";
 
 import {UserListService} from './users/user-list.service';
+import {TodoListService} from "./todos/todo-list.service";
 import {Routing} from './app.routes';
 import {APP_BASE_HREF} from '@angular/common';
 
 import {CustomModule} from './custom.module';
 import {UserComponent} from './users/user.component';
-import {TodoListComponent} from "./todos/todo-list.component";
 import {TodoComponent} from './todos/todo.component';
 
 
@@ -36,6 +37,7 @@ import {TodoComponent} from './todos/todo.component';
   ],
   providers: [
     UserListService,
+    TodoListService,
     {provide: APP_BASE_HREF, useValue: '/'},
     {provide: MATERIAL_COMPATIBILITY_MODE, useValue: true}
   ],
